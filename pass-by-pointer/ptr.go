b327022cf3a3858b90e9cae2eb50fccb3bda02e4
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2024-01-03 23:19:15 +0530

    cleaner code

--- pass-by-pointer/ptr.go
+++ pass-by-pointer/ptr.go
@@ -1,11 +1,10 @@
 package main
 
-type BigStruct struct{ buf [1 << 16]byte }
+type BigStruct struct { buf [1 << 16]byte }
 
 var obj BigStruct = BigStruct{}
 
 func fooPBV(obj BigStruct) {}
-
 func fooPBP(obj *BigStruct) {}
 
 func foo() {
